e46bbcdce77f8b72c9a584a79b0f3b51aed62dce
Reginald Amedee <reginald.amedee@gmail.com>
2022-05-13 19:53:54 -0400

    added error if pokemon does not exist

--- src/App.js
+++ src/App.js
@@ -1,47 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
 import PokemonForm from './components/PokemonForm/PokemonForm';
-import PokemonList from './components/PokemonList.js/PokemonList';
-import React, {useState, useEffect} from 'react';
+import PokemonList from './components/PokemonList/PokemonList';
+import Header from './components/Header/Header';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
   const [pokemon, setPokemon] = useState('');
   const [currentPokemon, setCurrentPokemon] = useState([]);
   const [responseObj, setResponseObj] = useState({});
+  const [request, setRequest] = useState()
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then(response => response.json())
-    .then((response) => {
-         setResponseObj(response)
-         // you have to store data as an array because that is what you're getting back
-         // typically, data is in an array of objects so you have to see what you're getting back
+      .then(response => {
+        if (response.ok) {
+          setRequest(true)
+          return response.json()
 
-         // look up how to pass child components to a parent component***
-         
-         console.log(responseObj)
-    })
-    
-    
-}, [pokemon]) 
+        } else {
+          setRequest(false)
+        }
+        throw new Error('Request Failed')
+      }, networkError => {
+        console.log(networkError.message)
+      })
+      .then(jsonResponse => {
+        return setResponseObj(jsonResponse)
+      })
+      .catch(function(error) {
+        console.log(error)
+        
 
-function onSubmitHandler (e) {
-  e.preventDefault()
-  setCurrentPokemon([responseObj])
-  
-}
+      })
 
-function onChangeHandler(e) {
-  setPokemon(e.target.value)
-}
+
+
+  }, [pokemon])
+
+  function onSubmitHandler(e) {
+    e.preventDefault()
+    setCurrentPokemon([responseObj])
+
+
+  }
+
+  function onChangeHandler(e) {
+    setPokemon(e.target.value.toLowerCase())
+  }
 
   return (
     <div className="App">
-      
-      <PokemonForm pokemon={pokemon} onChangeHandler={onChangeHandler} onSubmitHandler={onSubmitHandler}/>
-      <PokemonList currentPokemon={currentPokemon}/>
-      
+      <header>
+        <Header />
+      </header>
+      <main>
+      <section>
+        <PokemonForm pokemon={pokemon} onChangeHandler={onChangeHandler} onSubmitHandler={onSubmitHandler} />
+        </section>
+      <section>
+        <PokemonList currentPokemon={currentPokemon} request={request}/>
+      </section>
+      </main>
+      <footer>
+        Page created by Reginald Jean Amedee
+      </footer>
     </div>
   );
 }
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -2,19 +2,24 @@ import React, {useState} from "react";
 
 
 const PokemonList = (props) => {
-    const { currentPokemon } = props;
+    const { currentPokemon, request } = props;
 
     return <>
+    {request === false ? <p>Please input a valid poken</p> :
     <div>
         {currentPokemon.map(item => {
-            return <div key={item.id}>
+            return (<div>
+                <React.Fragment key={item.id}>
                 <p>{item.name}</p>
                 
+                
+                </React.Fragment>
             </div>
+            )
         })}
     </div>
 
-    
+}
     </>
 }
 
