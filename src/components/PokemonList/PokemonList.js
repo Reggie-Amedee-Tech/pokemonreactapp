4d91aef2d4a2384436164cb3706be21b0f9edb6e
Reginald Amedee <reginald.amedee@gmail.com>
2022-05-14 14:51:45 -0400

    added some styling

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import classes from './PokemonList.module.css'
 
 
@@ -6,43 +6,35 @@ const PokemonList = (props) => {
     const { currentPokemon, request } = props;
 
     return <>
-    {request === false ? <p>Please input a valid poken</p> :
-    <div>
-        {currentPokemon.map(item => {
-            return (<div>
-                <React.Fragment key={item.id}>
-                <div className={classes.TopShell}>
-                    <div>
-                        <img src={item.sprites.front_default} alt='picture of pokemon' className={classes.Img}></img>
+        {request === false ? <p>Please input a valid pokemon</p> :
+            <div>
+                {currentPokemon.map(item => {
+                    return (<div>
+                        <React.Fragment key={item.id}>
+                            <div className={classes.TopShell}>
+                                <div className={classes.Pic}>
+                                    <img src={item.sprites.front_default} alt='picture of pokemon' className={classes.Img}></img>
+                                </div>
+                                <div className={classes.Bottomshell}>
+                                    <h1 className={classes.H1}>Pokemon Name</h1>
+                                    <p className={classes.P}>{item.name.toUpperCase()}</p>
+                                    <h1 className={classes.H1}>First Appeared in Pokemon</h1>
+                                    <p className={classes.P}>{item.game_indices[0].version.name.toUpperCase()}</p>
+                                    <div>
+                                        <h1 className={classes.H1}>Abilities</h1>
+                                            {item.abilities.map((pitem) => {
+                                                return <p className={classes.P}>{pitem.ability.name}</p>
+                                            })}  
+                                    </div>
+                                </div>
+                            </div>
+                        </React.Fragment>
                     </div>
-                    <div>
-
-                    <p>Pokemon Name: {item.name}</p>
-                    <p>First Appearance: Pokemon - {item.game_indices[0].version.name}</p>
-
-                    <div>
-
-                    <h3 className={classes.H3}>Abilities</h3>
-                    <ol className={classes.Ol}>
-                        <li>{item.abilities[0].ability.name}</li>
-                        <li>{item.abilities[1].ability.name}</li>
-                    </ol>
-                    </div>
-
-                    </div>
-
-                    
-
-                </div>
-                
-                
-                </React.Fragment>
+                    )
+                })}
             </div>
-            )
-        })}
-    </div>
 
-}
+        }
     </>
 }
 
