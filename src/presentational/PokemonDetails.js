2370868831240489b4323c17ab391b189f78abb9
Reginald Amedee <reginald.amedee@gmail.com>
2022-06-15 12:50:25 -0400

    Added pokemon detail css rough sketch

--- src/presentational/PokemonDetails.js
+++ src/presentational/PokemonDetails.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import classes from '../cssModules/PokemonDetails.module.css'
 
 
 
@@ -7,22 +8,42 @@ const PokemonDetails = (props) => {
     const { detailedPokeData } = props;
     const [pokemonGames, setPokemonGames] = useState([])
 
-
-    
-    
-    
-
     return (
         <div>
-            {detailedPokeData.loading ? <h2>Please reload pokemon information</h2> : detailedPokeData.loading === false ? <div>
-            <img src={detailedPokeData.detailedPokemon.sprites.front_shiny} alt="pokemon avatar"></img>
-            <p>{detailedPokeData.detailedPokemon.height}</p>
-            <p>{detailedPokeData.detailedPokemon.game_indices.map(item => item.version.name)[0]}</p>
-            <p>{detailedPokeData.detailedPokemon.abilities.map(item => {
-                return <p>{item.ability.name}</p>
-            })}</p>
-            </div> : <h2>Pokemon does not exist</h2> }
-            
+            {detailedPokeData.loading ? <h2>Please reload pokemon information</h2> : detailedPokeData.loading === false ?
+                <div className={classes.CardDiv}>
+                    <div className={classes.Container}>
+                        <div className={classes.PokemonNameDiv}>
+                            <h2 className={classes.PokemonName}>{detailedPokeData.detailedPokemon.name}</h2>
+                        </div>
+                        <div className={classes.PokemonPictureDiv}>
+                            <div className={classes.PokemonPictureContainer}>
+                                <img src={detailedPokeData.detailedPokemon.sprites.other['official-artwork'].front_default} alt="pokemon avatar" className={classes.Img}></img>
+                            </div>
+                        </div>
+                        <div className={classes.BottomDiv}>
+                            <div className={classes.BottomContainer}>
+                                <div className={classes.AboutContainer}>
+                                    <h2 className={classes.BottomContainerTitle}>About...</h2>
+                                </div>
+                                <div className={classes.AboutDetailsContainer}>
+                                    <div className={classes.AboutLeft}>
+                                        <h4 className={classes.BottomContainerTitle}>Game Debute:</h4>
+                                        <p className={classes.PTag}>{detailedPokeData.detailedPokemon.game_indices.map(item => item.version.name)[0]}</p>
+                                    </div>
+                                    <div className={classes.AboutRight}>
+                                        <h4 className={classes.BottomContainerTitle}>Abilities:</h4>
+                                        <p>{detailedPokeData.detailedPokemon.abilities.map(item => {
+                                            return <p className={classes.PTag}>{item.ability.name}</p>
+                                        })}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> :
+                <h2>Pokemon does not exist</h2>}
+
         </div>
     );
 };
