857fa9c524d11b495025d4fdaddd32bf6080d7a4
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-12-15 14:43:52 -0500

    added pokemon data on the pokemondetails component

--- src/presentational/PokemonDetailFromList.js
+++ src/presentational/PokemonDetailFromList.js
@@ -1,23 +1,69 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import classes from '../cssModules/PokemonDetails.module.css'
 
 
 const PokemonDetailFromList = (props) => {
+    const [pokemonDetails, setPokemonDetails] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
     const location = useLocation()
     const pokemonName = location.pathname.slice(35)
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
-        .then(res => console.log(res))
-        .catch(err => console.log(err.message))
+            .then(res => {
+                setPokemonDetails([res.data])
+                setLoaded(true)
+            })
+            .catch(err => console.log(err.message))
     })
 
+    return (
+        <div>{loaded === true ? <div>
+            {pokemonDetails.map(details => {
+                return <div className={classes.CardDiv}>
+                <div className={classes.CardContainer}>
+                    <div className={classes.PokemonNameDiv}>
+                        <h2 className={classes.PokemonName}>{details.name}</h2>
+                    </div>
+                    <div className={classes.PokemonPictureDiv}>
+                        <div className={classes.PokemonPictureContainer}>
+                            <img src={details.sprites.other['official-artwork'].front_default} alt="pokemon avatar" className={classes.Img}></img>
+                        </div>
+                    </div>
+                    <div className={classes.BottomDiv}>
+                        <div className={classes.BottomContainer}>
+                            <div className={classes.AboutContainer}>
+                                <h2 className={classes.BottomContainerTitle}>About...</h2>
+                            </div>
+                            <div className={classes.AboutDetailsContainer}>
+                                <div className={classes.AboutLeft}>
+                                    <h4 className={classes.BottomContainerTitle}>Game Debute:</h4>
+                                    <p className={classes.PTag}>{details.game_indices.map(item => item.version.name)[0]}</p>
+                                </div>
+                                <div className={classes.AboutRight}>
+                                    <h4 className={classes.BottomContainerTitle}>Abilities:</h4>
+                                    <p>{details.abilities.map(item => {
+                                        return <p className={classes.PTag}>{item.ability.name}</p>
+                                    })}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button onClick={() => navigate(-1)} className={classes.Button}>Go Back</button>
+                
+            </div>
+            })}
+        </div> : <div>
+            <h1>Loading...</h1>
+        </div>}
 
-  return (
-    <div>PokemonDetailFromList</div>
-  )
+        </div>
+    )
 }
 
 export default PokemonDetailFromList
