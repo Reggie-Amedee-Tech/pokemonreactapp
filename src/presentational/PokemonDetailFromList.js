8b78d7ae9c9e351e6b828206f84b5b8af95e1f0a
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2022-12-21 12:09:28 -0500

    added type in pokemon card page

--- src/presentational/PokemonDetailFromList.js
+++ src/presentational/PokemonDetailFromList.js
@@ -20,6 +20,42 @@ const PokemonDetailFromList = (props) => {
             .catch(err => console.log(err.message))
     })
 
+    const typeSelector = (type) => {
+        if (type === 'electric') {
+            return <h2 style={{ backgroundColor: "#F7D02C" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'normal') {
+            return <h2 style={{ backgroundColor: "#A8A77A" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'poison') {
+            return <h2 style={{ backgroundColor: "#A33EA1" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'flying') {
+            return <h2 style={{ backgroundColor: "#A98FF3" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'ground') {
+            return <h2 style={{ backgroundColor: "#E2BF65" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'fire') {
+            return <h2 style={{ backgroundColor: "#EE8130" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'bug') {
+            return <h2 style={{ backgroundColor: "#A6B91A" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'ghost') {
+            return <h2 style={{ backgroundColor: "#735797" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'psychic') {
+            return <h2 style={{ backgroundColor: "#F95587" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'ice') {
+            return <h2 style={{ backgroundColor: "#96D9D6" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'dragon') {
+            return <h2 style={{ backgroundColor: "#6F35FC" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'dark') {
+            return <h2 style={{ backgroundColor: "#705746" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'fairy') {
+            return <h2 style={{ backgroundColor: "#D685AD" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'rock') {
+            return <h2 style={{ backgroundColor: "#B6A136" }} className={classes.PokemonType}>{type}</h2>
+        } else if (type === 'water') {
+            return <h2 style={{ backgroundColor: "#6390F0" }} className={classes.PokemonType}>{type}</h2>
+        } else {
+            return <h2>{type}</h2>
+        }
+    }
+
     return (
         <div>{loaded === true ? <div>
             {pokemonDetails.map(details => {
@@ -27,6 +63,7 @@ const PokemonDetailFromList = (props) => {
                 <div className={classes.CardContainer}>
                     <div className={classes.PokemonNameDiv}>
                         <h2 className={classes.PokemonName}>{details.name}</h2>
+                        {typeSelector(details.types[0].type.name)}
                     </div>
                     <div className={classes.PokemonPictureDiv}>
                         <div className={classes.PokemonPictureContainer}>
